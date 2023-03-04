463e8665d40f298c0992c512cd9e9149ea7e8240
William Tian <tianyuan01119@gmail.com>
2023-03-05 00:58:33 +1300

    add entries and init mock api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import initFetchMock from "./services/mocks/mock-api-sorce";
+import fetchMock from "fetch-mock";
+import { LoginContainer } from './containers/LoginContainer';
 import './App.css';
+import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import { DashboardContainer } from './containers/DashboardContainer';
+
+initFetchMock(fetchMock);
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/dashboard',
+      element: <DashboardContainer />,
+    },
+    {
+      path: '/',
+      element: <LoginContainer />
+    }
+  ])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
